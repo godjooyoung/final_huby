cb32770f387f093ccdfa507143f119f57ecb3579
JOO <JOO@DESKTOP-R2ICKPJ>
2020-06-22 22:12:40 +0900

    comit..,

--- src/main/java/co/huby/prj/board/service/EmploymentService.java
+++ src/main/java/co/huby/prj/board/service/EmploymentService.java
@@ -12,13 +12,23 @@ public interface EmploymentService {
 
 	Map SelectMatchedEmploymentList(Map vo) throws Exception;
 
-	List<Map> resumeAll(Map vo) throws Exception;
+	List<Map> resumeAll(MemberVo vo) throws Exception;
 
-	List<Map> videoAll(Map vo) throws Exception;
+	List<Map> videoAll(MemberVo vo) throws Exception;
 
-	List<Map> skillsAll(Map vo) throws Exception;
+	List<Map> skillsAll(MemberVo vo) throws Exception;
 
-	List<Map> careerAll(Map vo) throws Exception;
+	List<Map> careerAll(MemberVo vo) throws Exception;
+	
+	List<Map> habitAll(MemberVo vo) throws Exception;
 
-	List<Map> employmentList(Map vo) throws Exception;
+	List<Map> employmentList(EmploymentsVo vo) throws Exception;
+
+	List<Map> empDetailList(EmploymentsVo vo) throws Exception;
+	
+	int applyInsert(Map vo) throws Exception;
+	
+	int applycheck(Map vo) throws Exception;
+	
+	List<Map> applyList(MemberVo vo) throws Exception;
 }
